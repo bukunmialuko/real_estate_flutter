0ea35a17dc59d313acaf8a63932755268a092085
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-05 20:47:09 +0100

    first commit 🚀🚀🚀

---
+++ android/app/src/main/kotlin/com/moniepoint/realestate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.moniepoint.realestate.superapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:superapp/app/app.dart';
+import 'package:superapp/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
