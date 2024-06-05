bdbe5801dcbe78cf79ca3b27cc7158718bdf2a8b
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-05 21:40:58 +0100

    feat: update app main files

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:superapp/app/app.dart';
 import 'package:superapp/bootstrap.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future<void> main() async {
+  return runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    await setPreferredOrientations();
+
+    await bootstrap(() => const App());
+  }, (error, stackTrace) {
+    debugPrint(stackTrace.toString());
+    debugPrint(error.toString());
+  });
+}
+
+Future<void> setPreferredOrientations() {
+  return SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 }
