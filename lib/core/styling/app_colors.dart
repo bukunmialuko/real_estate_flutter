e6de6ad7883286b39426a5d18820902232f22cd0
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-05 21:39:50 +0100

    feat: setup app colors

---
+++ lib/core/styling/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const black = Color(0xff1c1c1c);
+  static const white = Color(0xffffffff);
+
+  static const lightGrey = Color(0xff727272);
+  static const darkGrey = Color(0xff2c2c2c);
+  static const darkOrange = Color(0xfffc9e12);
+  static const lightOrange = Color(0xfffc9e12);
+}
