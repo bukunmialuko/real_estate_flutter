991291e48ee1a49cac311f38afe4c55e10f3e788
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-05 20:50:23 +0100

    chore: add helper scripts

---
+++ scripts/clean_get_gen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+pwd
+
+(cd .. && flutter clean)
+(cd .. && flutter pub get)
+(flutter pub run build_runner build --delete-conflicting-outputs)
+
+exit 0
---
+++ scripts/generate_required_code.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+(flutter pub run build_runner build --delete-conflicting-outputs)
+
+exit 0
