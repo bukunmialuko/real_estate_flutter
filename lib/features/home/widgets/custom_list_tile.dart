f6af098099f0cace42550ed2a574395f510ba73f
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-06 15:39:53 +0100

    feat: animate list item slider

--- lib/features/home/widgets/custom_list_tile.dart
+++ lib/features/home/widgets/custom_list_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:superapp/generated/assets.gen.dart';
 
@@ -32,8 +33,12 @@ class CustomListTile extends StatefulWidget {
 }
 
 class _CustomListTileState extends State<CustomListTile> {
+  bool expand = false;
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
       padding: EdgeInsets.only(left: 18.w, right: 18.w),
       decoration: BoxDecoration(
@@ -45,9 +50,12 @@ class _CustomListTileState extends State<CustomListTile> {
       ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(
-            height: 40.h,
+          AnimatedContainer(
+            height: 42.h,
+            width: expand ? screenWidth : 42.h,
+            duration: const Duration(milliseconds: 2000),
             child: Stack(
               children: [
                 Center(
@@ -75,54 +83,37 @@ class _CustomListTileState extends State<CustomListTile> {
                       color: Colors.black,
                     ),
                   ),
-                ),
-                Center(
-                  child: Container(
-                    height: 38.h,
-                    width: 38.h,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.8),
-                      shape: BoxShape.circle,
+                ).animate().fadeIn(delay: const Duration(milliseconds: 4000)),
+                Row(
+                  mainAxisAlignment: expand
+                      ? MainAxisAlignment.spaceBetween
+                      : MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(),
+                    Align(
+                      child: Container(
+                        height: 38.h,
+                        width: 38.h,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Icon(
+                          size: 18,
+                          Icons.chevron_right_rounded,
+                        ),
+                      ),
                     ),
-                    child: const Icon(
-                      size: 18,
-                      Icons.chevron_right_rounded,
-                    ),
-                  ),
+                  ],
                 ),
               ],
             ),
-          ),
+          ).animate(onComplete: (s) {
+            setState(() {
+              expand = true;
+            });
+          }).scale(delay: const Duration(milliseconds: 2000)),
           SizedBox(height: 10.h),
-          // Container(
-          //   height: 40.h,
-          //   width: double.infinity,
-          //   margin: EdgeInsets.only(bottom: 10.h, left: 18.w, right: 18.w),
-          //   decoration: BoxDecoration(
-          //     color: Colors.white.withOpacity(0.8),
-          //     borderRadius: BorderRadius.circular(56.r),
-          //   ),
-          //   child: Stack(
-          //     children: [
-          //       const Center(
-          //         child: Text('Gladkova., 25'),
-          //       ),
-          //       Container(
-          //         height: 40.h,
-          //         width: double.infinity,
-          //         margin:
-          //             EdgeInsets.only(bottom: 10.h, left: 18.w, right: 18.w),
-          //         decoration: BoxDecoration(
-          //           color: Colors.white.withOpacity(0.8),
-          //           borderRadius: BorderRadius.circular(56.r),
-          //         ),
-          //         child: const Icon(
-          //           Icons.chevron_right_rounded,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
         ],
       ),
     );
