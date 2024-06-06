7ec0d150d0650587a672cb86af47567aa2baaf56
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-06 15:06:15 +0100

    feat: animate bottom nav

--- lib/features/home/widgets/bottom_nav/custom_bottom_nav_bar.dart
+++ lib/features/home/widgets/bottom_nav/custom_bottom_nav_bar.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:superapp/core/styling/app_colors.dart';
 
@@ -20,6 +23,7 @@ class CustomBottomNavBar extends StatelessWidget {
       child: Container(
         height: 60.h,
         color: Colors.transparent,
+        margin: EdgeInsets.only(bottom: Platform.isAndroid ? 10.h : 4.h),
         child: Center(
           child: Container(
             width: 266.w,
@@ -43,7 +47,13 @@ class CustomBottomNavBar extends StatelessWidget {
             ),
           ),
         ),
-      ),
+      ).animate().slideY(
+            delay: const Duration(milliseconds: 5000),
+            duration: const Duration(milliseconds: 2000),
+            begin: 5,
+            end: 0,
+            curve: Curves.easeIn,
+          ),
     );
   }
 }
